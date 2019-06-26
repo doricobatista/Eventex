b729307c60cea91e077f77c1f480ed6062a4338f
dorico <doricobatista@gmail.com>
2019-06-26 09:40:17 -0300

    Atualização: urls.py modificado

--- sistema/urls.py
+++ sistema/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-import sistema.core.views
+from sistema.core.views import home
 
 urlpatterns = [
-    path('', sistema.core.views.home),
+    path('', home),
     path('admin/', admin.site.urls),
 ]
