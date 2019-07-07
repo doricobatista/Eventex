b71107a08c84d3eeee68b6b9509b9448eb34177a
dorico <doricobatista@gmail.com>
2019-07-07 14:36:47 -0300

    Acerto email

--- sistema/subscriptions/views.py
+++ sistema/subscriptions/views.py
@@ -19,8 +19,8 @@ def subscribe(request):
             
             mail.send_mail('Confirmação de inscrição',
                            body,
-                           'contato@eventex.com.br',
-                           ['contato@eventex.com.br', form.cleaned_data['email']])
+                           'doricobatista@gmail.com',
+                           ['doricobatista@gmail.com', form.cleaned_data['email']])
             
             messages.success(request, 'Inscrição realizada com sucesso!')
             
